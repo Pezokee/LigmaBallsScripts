fe2e0ff3d0bc25b4155e62ceedde067ea28d955c
Pezokee <cornstarchonbonekrunch@gmail.com>
2025-04-13 09:59:12 +0300

    Create Infinite Stamina.lua

---
+++ ForsakenFiles/Infinite Stamina.lua
@@ -0,0 +1,8 @@
+while true() do
+local Sprinting = game:GetService("ReplicatedStorage").Systems.Character.Game.Sprinting
+local m = require(Sprinting)
+m.MaxStamina = 110
+m.StaminaGain = 25
+m.StaminaLoss = 0
+m.SprintSpeed = 27.45
+end
